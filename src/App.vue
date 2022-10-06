4d0a7efa5357a8a26973c8d9948f581c1e9e5892
emilia-t <sophietwilight@qq.com>
2022-10-06 11:20:10 +0800

    lost

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,25 @@
 <template>
   <div id="app">
     <!--数据层-->
+    <!--用于显示和编辑数据用-->
     <data-layer>
 
     </data-layer>
     <!--标尺层-->
+    <!--用于显示上策和左侧经纬度标尺-->
+    <ruler-layer>
+
+    </ruler-layer>
     <!--背景层-->
+    <!--用于显示背景，例如谷歌给地图-->
+    <background-layer>
+
+    </background-layer>
     <!--新建层-->
+    <!--用于创建新的要素-->
+    <create-layer>
+
+    </create-layer>
   </div>
 </template>
 
@@ -16,9 +29,10 @@
 import dataLayer from "./components/dataLayer";
 import rulerLayer from "./components/rulerLayer";
 import backgroundLayer from "./components/backgroundLayer";
+import createLayer from "./components/createLayer";
 export default {
   name: 'App',
-  components: {dataLayer,rulerLayer,backgroundLayer},
+  components: {dataLayer,rulerLayer,backgroundLayer,createLayer},
   data(){
     return {
 
---
+++ src/components/createLayer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="createLayer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "createLayer"
+}
+</script>
+
+<style scoped>
+
+</style>
