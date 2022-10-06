af8452b96a5350088b24e4f50e36ed61c6601652
emilia-t <sophietwilight@qq.com>
2022-10-06 11:35:16 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -36,15 +36,49 @@
 
 <script>
 //import MapBox from "./components/mapBox";
+/*
+数据层
+用于显示和编辑数据用
+*/
 import dataLayer from "./components/dataLayer";
+/*
+标尺层
+用于显示上策和左侧经纬度标尺
+*/
 import rulerLayer from "./components/rulerLayer";
+/*
+背景层
+用于显示背景，例如谷歌给地图
+*/
 import backgroundLayer from "./components/backgroundLayer";
+/*
+<!--新建层-->
+<!--用于创建新的要素-->
+*/
 import createLayer from "./components/createLayer";
+/*
+<!--控制层-->
+<!--用于控制一些操作用，例如编辑按钮、清空按钮-->
+*/
 import controlLayer from "./components/controlLayer";
+/*
+<!--面板层-->
+<!--用于展示图层数据的-->
+*/
 import panelLayer from "./components/panelLayer";
+/*
+通信层
+用于各组件之间通信交流
+*/
+import communicationLayer from "./components/communicationLayer";
 export default {
   name: 'App',
-  components: {dataLayer,rulerLayer,backgroundLayer,createLayer,controlLayer,panelLayer},
+  components: {
+    //普通图层
+    dataLayer,rulerLayer,backgroundLayer,createLayer,controlLayer,panelLayer,
+    //特殊图层
+    communicationLayer
+  },
   data(){
     return {
 
