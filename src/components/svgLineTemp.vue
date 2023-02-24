fa2725329d67010315d925b2b55ec200740b6cdd
emilia-t <sophietwilight@qq.com>
2023-02-24 11:30:59 +0800

    “fix”

---
+++ src/components/svgLineTemp.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "svgLineTemp"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/svgPointP0.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "svgPointP0"
+}
+</script>
+
+<style scoped>
+
+</style>
