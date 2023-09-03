5c5c80fe3eddc1de3b6f4f810c7381bf329c8462
emilia-t <69798413+emilia-t@users.noreply.github.com>
2023-09-03 11:04:59 +0800

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ export default {
   //连接您的地图服务器的服务器地址
   //__SERVER_ADDRESS__:""
   //__SERVER_ADDRESS__:""
-  bottomAnchorTextA:'滇公网安备 53062602000121号',
-  bottomAnchorTextB:'滇ICP备20002587号-2',
-  bottomAnchorTextC:'53062602000121',
+  bottomAnchorTextA:'华夏网安备 OOOOOOOOOOOOOO号',
+  bottomAnchorTextB:'华XXX备00000000号',
+  bottomAnchorTextC:'OOOOOOOOOOOOOO',
 }
